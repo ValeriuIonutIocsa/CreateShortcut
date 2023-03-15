1870d254af6ea2ddb30e627ef3f821e5d48fbbe1
Valeriu Ionut Iocsa <valeriuionutiocsa@gmail.com>
2023-03-15 10:52:23 +0200

    improved deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 @echo off
 
 dotnet.exe publish CreateShortcut.csproj --self-contained -o C:\IVI\Apps\Scripts\General\CreateShortcut -c Release -r win-x64 ^
-/p:PublishTrimmed=true /p:TrimMode=Link /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
+/p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
